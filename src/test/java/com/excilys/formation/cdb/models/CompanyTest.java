7980ff58dc6694e6a2409ff45f4825e7f9c4dccf
Suline <scai@excilys.com>
2020-03-16 17:37:08 +0100

    UPD update maven structure

---
+++ src/test/java/com/excilys/formation/cdb/models/CompanyTest.java
@@ -0,0 +1,14 @@
+package com.excilys.formation.cdb.models;
+
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class CompanyTest {
+
+	@Test
+	public void test() {
+		assertNull(null);
+	}
+
+}
