a0766ccc1df830310ad1c6f8341f1a1b43fef110
Suline <scai@excilys.com>
2020-06-30 11:49:31 +0200

    UPD Remove redondant spring dependencies

--- src/main/java/com/excilys/formation/java/cdb/spring/SpringConfiguration.java
+++ src/main/java/com/excilys/formation/java/cdb/spring/SpringConfiguration.java
@@ -6,9 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.context.AbstractContextLoaderInitializer;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
